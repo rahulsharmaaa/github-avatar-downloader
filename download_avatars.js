0fe071f58e906423a4f41c69e9158e2e81803682
Rahul Sharma <rahul__sharma@outlook.com>
2018-07-31 02:31:36 +0000

    updated download_avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,17 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request'
+      }
+    };
+  
+    request(options, function(err, res, body) {
+      cb(err, body);
+      curl -i -H 'Authorization: token YOUR_TOKEN_HERE' https://api.github.com/repos/jquery/jquery/contributors
+    });
+  }
